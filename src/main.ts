0d7f72838f27f519c54fa5f2fd5f266ed6d1df92
seokwon1108 <swseong@vvnst.com>
2024-07-22 16:01:22 +0900

    tailwind setting

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import router from "./router";
 import { createPinia } from "pinia";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import "./assets/scss/global.scss";
 
 const pinia = createPinia();
 
